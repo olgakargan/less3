0e28d8a6560a4ca41c436a56d28488d40c7adef8
ольга <olgakargan72@gmail.com>
2022-07-14 00:39:50 +0400

    выполнила дз

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Пишем наше ДЗ");
-        System.out.println("Отправляем ДЗ");
-    }
-}
+
+
+     public class Main {
+         public static void main(String[] args) {
+
+             //Задание1
+
+             int clientIOS = 0;
+             int clientAndroid = 1;
+
+             if (clientIOS == 0 && clientAndroid == 1) {
+             }
+             {
+                 System.out.println("Install the iOS version of the app by following the link");
+                 System.out.println("Install the android version of the app by following the link");
+             }
+
+             //Задание2
+             int clientDeviceYearIOS = 2015;
+             int clientDeviceYearAndroid = 2015;
+
+             if (clientDeviceYearIOS <= 2015 || clientDeviceYearAndroid <= 2015) {
+
+             }
+             System.out.println("Install thlite version of the Android app by following the link");
+             System.out.println("Install the lite version of the IOS app by following the link");
+
+
+             if (clientDeviceYearAndroid > 2015 || clientDeviceYearAndroid > 2015) {
+
+             }
+             System.out.println("Install the Android app by following the link");
+             System.out.println("Install the IOS app by following the link");
+
+
+         }
+
+        }
+
+
